6aa40bfa7e97efaefc862ee915afc367f99b5dc3
SadieJS <rosycapetown@protonmail.com>
2022-04-05 21:01:00 +0200

    created diet items component

---
+++ src/app/diet-item/diet-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'diet-item',
+  templateUrl: './diet-item.component.html',
+  styleUrls: ['./diet-item.component.css']
+})
+export class DietItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
